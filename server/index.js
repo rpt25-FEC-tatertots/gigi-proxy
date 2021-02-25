da463cbcfa9cb364b38bee8c00f31212eb46db8a
gigicupp <gigicupp@gmail.com>
2021-02-24 20:06:00 -0800

    optimization

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const cors = require('cors');
 const axios = require('axios');
+const compression = require('compression');
 
 const app = express();
-
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
